5ef8144f5ee43ee366ae2ccc642b2579b78beeca
Wojciech Musial <wojciechmusial23@gmail.com>
2018-11-19 11:19:13 +0100

    Added general command sequence handlers + step forward handlers.

--- src/game/controller/GameOverviewController.java
+++ src/game/controller/GameOverviewController.java
@@ -1,21 +1,50 @@
 package game.controller;
 
 import game.model.Board;
+import game.model.CommandSequence;
+import game.model.command.StepForwardCommand;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 
 public class GameOverviewController {
 
     private GameAppController appController;
-    private Board data;
+    private Board boardData;
+    private CommandSequence commandSequence;
 
     @FXML
     private void initialize() {
+        this.commandSequence = new CommandSequence();
+    }
+
+    @FXML
+    private void handleAddStepForwardAction(ActionEvent event) {
+        commandSequence.addCommand(new StepForwardCommand(boardData.getTurtle()));
+        System.out.println("Step forward event fired.");
+    }
+
+    @FXML
+    private void handleRemoveLastCommandAction(ActionEvent event) {
+        commandSequence.removeLastCommand();
+        System.out.println("Remove last command event fired.");
+    }
+
+    @FXML
+    private void handleClearCommandSequenceAction(ActionEvent event) {
+        commandSequence.clear();
+        System.out.println("Clear sequence event fired.");
+    }
+
+    @FXML
+    private void handleExecuteCommandSequenceAction(ActionEvent event) {
+        commandSequence.execute();
+        System.out.println("Execute event fired.");
     }
 
     public void setData(Board board) {
 
-        this.data = board;
+        this.boardData = board;
 
     }
 
